556e1743c0c213187e11ea941800820e22daae0a
LAPTOP-IG3DGFR4\simon <simon123kracht@gmail.com>
2021-07-06 13:09:17 +0200

    Added Team Setting RPCcalls

--- Simon&SandroMansUltiMulti/Assets/Scripts/PlayerManager.cs
+++ Simon&SandroMansUltiMulti/Assets/Scripts/PlayerManager.cs
@@ -22,20 +22,26 @@ public class PlayerManager : MonoBehaviourPun
 
     List<TMP_Text> playerTxts;
     List<PlayerType> playerTypes;
+    RoomPlayerSettings settings;
+    private List<string> playerNames;
 
     private void Start()
     {
-        PhotonNetwork.LocalPlayer.NickName = "Jeff";
-        Player1 = PlayerType.NPC;
-        Player2 = PlayerType.NPC;
-        Player3 = PlayerType.NPC;
-        Player4 = PlayerType.NPC;
-        playerTypes = new List<PlayerType>();
 
-        playerTypes.Add(Player1);
-        playerTypes.Add(Player2);
-        playerTypes.Add(Player3);
-        playerTypes.Add(Player4);
+        settings = new RoomPlayerSettings(PlayerType.NPC,"NPC", PlayerType.NPC, "NPC", PlayerType.NPC, "NPC", PlayerType.NPC, "NPC");
+        PhotonNetwork.LocalPlayer.NickName = "Jeff";
+
+        playerNames = new List<string>();
+        playerNames.Add(settings.Player1Name);
+        playerNames.Add(settings.Player2Name);
+        playerNames.Add(settings.Player3Name);
+        playerNames.Add(settings.Player4Name);
+
+        playerTypes = new List<PlayerType>();
+        playerTypes.Add(settings.Player1);
+        playerTypes.Add(settings.Player2);
+        playerTypes.Add(settings.Player3);
+        playerTypes.Add(settings.Player4);
 
         playerTxts = new List<TMP_Text>();
         playerTxts.Add(player1Txt);
@@ -56,12 +62,13 @@ public class PlayerManager : MonoBehaviourPun
                     if(playerTxts[i].text == PhotonNetwork.LocalPlayer.NickName)
                     {
                         playerTypes[i] = PlayerType.NPC;
-                        playerTxts[i].text = "NPC";
+                        playerNames[i] = "NPC";
                     }
                 }
             }
             playerTypes[id] = PlayerType.Player;
-            playerTxts[id].text = PhotonNetwork.LocalPlayer.NickName; 
+            playerNames[id] = PhotonNetwork.LocalPlayer.NickName;
+            OnUIClicked();
 
         }
         else if(playerTypes[id] == PlayerType.Player)
@@ -69,9 +76,64 @@ public class PlayerManager : MonoBehaviourPun
             if(playerTxts[id].text == PhotonNetwork.LocalPlayer.NickName)
             {
                 playerTypes[id] = PlayerType.NPC;
-                playerTxts[id].text = "NPC";
+                playerNames[id] = "NPC";
+                OnUIClicked();
             }
         }
     }
+    public void OnUIClicked()
+    {
+        photonView.RPC("DrawUIs", RpcTarget.All, settings);
+    }
 
+    [PunRPC]
+    public void DrawUIs(RoomPlayerSettings _settings)
+    {
+        settings = _settings;
+        player1Txt.text = settings.Player1Name;
+        player2Txt.text = settings.Player2Name;
+        player3Txt.text = settings.Player3Name;
+        player4Txt.text = settings.Player4Name;
+    }
+}
+public class RoomPlayerSettings
+{
+    private PlayerType player1;
+    public PlayerType Player1 { get => player1; set => player1 = value; }
+
+    private string player1Name;
+    public string Player1Name { get => player1Name; set => player1Name = value; }
+
+    private PlayerType player2;
+    public PlayerType Player2 { get => player2; set => player2 = value; }
+
+    private string player2Name;
+    public string Player2Name { get => player2Name; set => player2Name = value; }
+
+    private PlayerType player3;
+    public PlayerType Player3 { get => player3; set => player3 = value; }
+
+    private string player3Name;
+    public string Player3Name { get => player3Name; set => player3Name = value; }
+
+    private PlayerType player4;
+    public PlayerType Player4 { get => player4; set => player4 = value; }
+
+    private string player4Name;
+    public string Player4Name { get => player4Name; set => player4Name = value; }
+
+    public RoomPlayerSettings(PlayerType _player1Type, string _player1Name, PlayerType _player2Type, string _player2Name, PlayerType _player3Type, string _player3Name, PlayerType _player4Type, string _player4Name)
+    {
+        player1 = _player1Type;
+        player1Name = _player1Name;
+
+        player2 = _player2Type;
+        player2Name = _player2Name;
+
+        player3 = _player3Type;
+        player3Name = Player3Name;
+
+        player4 = _player4Type;
+        Player4Name = player4Name;
+    }
 }
