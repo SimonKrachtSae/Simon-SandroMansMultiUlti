a9ec8e3ce3210ad0f79a15df001ccc12d913f0d1
SandroKaz <aleksandrekazakhishvili@gmail.com>
2021-07-09 16:59:35 +0400

    Added Player Script settings

--- Simon&SandroMansUltiMulti/Assets/Scripts/MyPlayer.cs
+++ Simon&SandroMansUltiMulti/Assets/Scripts/MyPlayer.cs
@@ -10,7 +10,9 @@ public class MyPlayer : MonoBehaviourPun, IPunObservable
     public int ID { get => id; set => id = value; }
     [SerializeField] private GameObject playerCam;
     private SpriteRenderer spriteRenderer;
-
+    private float health = 100f;
+    
+    
     private Team team;
     
 
@@ -33,8 +35,29 @@ public class MyPlayer : MonoBehaviourPun, IPunObservable
     }
     void Update()
     {
-        
+        if (photonView.IsMine)
+        {
+            Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+            transform.position += input * Time.fixedDeltaTime * 5f;
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameObject bullet = PhotonNetwork.Instantiate("BulletPrefab", transform.position + transform.up * 2f, Quaternion.identity);
+                BulletScript bulletScript = bullet.GetComponent<BulletScript>();
+                bulletScript.SetLocalPlayer(this);
+                bullet.GetComponent<Rigidbody2D>().velocity = transform.up * Time.fixedDeltaTime * 100f;              
+            }
+            //RotateTowards(transform, Input.mousePosition, 10f);
+            
+        }
     }
+    /*private void RotateTowards(Transform transform, Vector3 target, float speed)
+    {
+        Vector3 targetDirection = new Vector3(target.x - transform.position.x, 0, target.z - transform.position.z);
+        float singleStep = speed * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(transform.up, targetDirection, singleStep, 0.0f);
+        Debug.DrawRay(transform.position, newDirection, Color.red);
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    }*/
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
 
@@ -56,4 +79,26 @@ public class MyPlayer : MonoBehaviourPun, IPunObservable
             spriteRenderer.color = Color.red;
         }
     }
+
+    public Team GetTeam()
+    {
+        return team;
+    }
+
+    public void Damage(float value)
+    {
+        photonView.RPC("RPC_Damage", RpcTarget.All, value);
+    }
+
+   [PunRPC]
+   public void RPC_Damage(float value)
+    {
+        health -= value;
+        if (health < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
 }
