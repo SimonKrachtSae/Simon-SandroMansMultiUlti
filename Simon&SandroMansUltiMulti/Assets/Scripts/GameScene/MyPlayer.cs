9732a007be126da41b49582403210ea03b68d828
LAPTOP-IG3DGFR4\simon <simon123kracht@gmail.com>
2021-07-11 18:01:27 +0200

    Fixed problems with playerparent

--- Simon&SandroMansUltiMulti/Assets/Scripts/GameScene/MyPlayer.cs
+++ Simon&SandroMansUltiMulti/Assets/Scripts/GameScene/MyPlayer.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+
 public class MyPlayer : PlayerParent
-{   
+{
+    [SerializeField] private protected GameObject playerCam;
+
     void Start()
     {
         if (!photonView.IsMine)
--- Simon&SandroMansUltiMulti/Assets/Scripts/GameScene/PlayerParent.cs
+++ Simon&SandroMansUltiMulti/Assets/Scripts/GameScene/PlayerParent.cs
@@ -11,33 +11,37 @@ public class PlayerParent : MonoBehaviourPun, IPunObservable
     private protected int id;
     public int ID { get => id; set => id = value; }
 
-    [SerializeField] private protected GameObject playerCam;
-    private protected SpriteRenderer spriteRenderer;
+    [SerializeField] private protected SpriteRenderer spriteRenderer;
 
     private protected Team team;
-    private protected void Awake()
-    {
-        spriteRenderer = transform.GetComponent<SpriteRenderer>();
-    }
     public void SetID(int _id)
     {
         if (_id < 2)
         {
-            photonView.RPC("RPC_SetID", RpcTarget.All, _id, Team.A, Color.blue);
+            photonView.RPC("RPC_SetID", RpcTarget.All, _id, Team.A);
         }
         else
         {
-            photonView.RPC("RPC_SetID", RpcTarget.All, _id, Team.B, Color.red);
+            photonView.RPC("RPC_SetID", RpcTarget.All, _id, Team.B);
         }
     }
     [PunRPC]
-    public void RPC_SetID(int _id, Team _team, Color _color)
+    public void RPC_SetID(int _id, Team _team)
     {
         id = _id;
         team = _team;
-        spriteRenderer.color = _color;
+
+        if(_team == Team.A)
+        {
+
+            spriteRenderer.color = Color.blue;
+        }
+        else
+        {
+            spriteRenderer.color = Color.red;
+        }
     }
-    public virtual void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
 
     }
