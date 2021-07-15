b9b59e9928bc11ce72a3d1e04d734453ffd6b408
LAPTOP-IG3DGFR4\simon <simon123kracht@gmail.com>
2021-07-15 09:51:38 +0200

    transform view classic

--- Simon&SandroMansUltiMulti/Assets/Scripts/GameScene/EntityBase.cs
+++ Simon&SandroMansUltiMulti/Assets/Scripts/GameScene/EntityBase.cs
@@ -86,15 +86,6 @@ public class EntityBase : MonoBehaviourPun, IPunObservable
     }
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        if(stream.IsWriting)
-        {
-            stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation);
-        }
-        else
-        {
-            transform.position = (Vector3)stream.ReceiveNext();
-            transform.rotation = (Quaternion)stream.ReceiveNext();
-        }
+
     }
 }
